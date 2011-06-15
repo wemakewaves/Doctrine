542dd1a1b73fe6bc59a51748124fbab07008fa70
UVd <info@ultravioletdesign.co.uk>
2011-06-15 18:42:44 +0100

    DateFunction

--- UVd/DoctrineFunction/DateFormat.php
+++ UVd/DoctrineFunction/DateFormat.php
@@ -1,21 +1,46 @@
 <?php
-/*
- */
-namespace UVd\DoctrineFunction;
+
+namespace UVd\DoctrineFunction\DoctrineFunction;
 
 use Doctrine\ORM\Query\Lexer;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 
 /**
- * Description of Hour
- *
- * @author Rob
+ * DateFormat
+ * 
+ * Allows Doctrine 2.0 Query Language to execute a MySQL DATE_FORMAT function
+ * You must boostrap this function in your ORM as a DQLFunction.
+ * 
+ * 
+ * DATE_FORMAT(TIMESTAMP,'%format') : @link http://dev.mysql.com/doc/refman/5.5/en/date-and-time-functions.html#function_date-format
+ * 
+ * 
+ * PLEASE REMEMBER TO CHECK YOUR NAMESPACE
+ * 
+ * @link labs.ultravioletdesign.co.uk
+ * @author Rob Squires <rob@ultravioletdesign.co.uk>
+ * 
+ * 
  */
 class DateFormat extends FunctionNode {
 
+    /*
+     * holds the timestamp of the DATE_FORMAT DQL statement
+     * @var mixed
+     */
     protected $dateExpression;
+    
+    /**
+     * holds the '%format' parameter of the DATE_FORMAT DQL statement
+     * @var string
+     */
     protected $formatChar;
 
+    /**
+     * getSql - allows ORM  to inject a DATE_FORMAT() statement into an SQL string being constructed
+     * @param \Doctrine\ORM\Query\SqlWalker $sqlWalker
+     * @return void 
+     */
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
         return 'DATE_FORMAT(' .
@@ -26,7 +51,10 @@ class DateFormat extends FunctionNode {
 
     }
 
-
+    /**
+     * parse - allows DQL to breakdown the DQL string into a processable structure
+     * @param \Doctrine\ORM\Query\Parser $parser 
+     */
     public function parse(\Doctrine\ORM\Query\Parser $parser)
     {
 
